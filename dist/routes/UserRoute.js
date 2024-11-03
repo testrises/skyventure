956ce236992595ad03311ff2c48bf3463d73bfb9
elquna <nsimamfon@gmail.com>
2024-11-03 06:25:15 +0100

    work on readme

--- dist/routes/UserRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var UserController_1 = require("../controllers/UserController");
-var router = require("express").Router();
-router.post('/create', UserController_1.createUser);
-router.post('/login', UserController_1.login);
-exports.default = router;
-//# sourceMappingURL=UserRoute.js.map
